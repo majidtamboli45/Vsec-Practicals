142c71c690ec92ae75393ac74e5227b0dfa6572d
majidtamboli45 <majidtamboli45@gmail.com>
2026-02-04 21:35:35 +0530

    This is simple code for the Calculator

---
+++ cal.py
@@ -0,0 +1,27 @@
+# Simple Calculator
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Choose operation:")
+print("+  Addition")
+print("-  Subtraction")
+print("*  Multiplication")
+print("/  Division")
+
+operation = input("Enter operation: ")
+
+if operation == "+":
+    print("Result:", num1 + num2)
+elif operation == "-":
+    print("Result:", num1 - num2)
+elif operation == "*":
+    print("Result:", num1 * num2)
+elif operation == "/":
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Error: Division by zero")
+else:
+    print("Invalid operation")
+
